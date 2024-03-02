3bc87cf2bf7a7b2cfcb0db8ad68c357118b31c6b
Abdelkrim Bourennane <abdelkrim.bournane@gmail.com>
2024-03-02 19:43:00 +0100

    Initial commit

---
+++ MemoryDumper.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+public class MemoryDumper : IDisposable
+{
+    private IntPtr _unmanagedBuffer;
+    private readonly int _size;
+    private readonly Stream _destinationStream;
+
+    public MemoryDumper(int size, string filePath)
+    {
+        this._size = size;
+        _unmanagedBuffer = Marshal.AllocHGlobal(size);
+        _destinationStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+    }
+
+    public void DumpToFile()
+    {
+        using (var bw = new BinaryWriter(_destinationStream))
+        {
+            byte[] buffer = new byte[_size];
+            Marshal.Copy(_unmanagedBuffer, buffer, 0, _size);
+            bw.Write(buffer);
+        }
+    }
+
+    public void Dispose()
+    {
+        if (_unmanagedBuffer != IntPtr.Zero)
+        {
+            Marshal.FreeHGlobal(_unmanagedBuffer);
+            _unmanagedBuffer = IntPtr.Zero;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// foreach (int _ in Enumerable.Range(0, 5))
+// {
+//     try
+//     {
+//         var pos = new Position(1, -1);
+//         Console.WriteLine(pos.X);
+//         Console.WriteLine(pos.Y);
+//     }
+//     catch
+//     {
+//         Console.WriteLine("Exception has been thrown");
+//     }
+// }
+//
+// while (Console.ReadKey().Key == ConsoleKey.C)
+// {
+//     GC.Collect();
+// }
+
+try
+{
+    DumpMemory();
+}
+catch
+{
+   Console.WriteLine("File exception caught."); 
+}
+
+while (Console.ReadKey().Key == ConsoleKey.C)
+{
+    GC.Collect();
+}
+
+void DumpMemory()
+{
+    using (var unmanagedMemoryHolder = new MemoryDumper(1024 * 1024 * 1024, ""))
+    {
+        unmanagedMemoryHolder.DumpToFile();
+    }
+}
+
+public class Position
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public Position(int x, int y)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(x, nameof(x));
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(y, nameof(y));
+
+        X = x;
+        Y = y;
+    }
+
+    ~Position()
+    {
+        Console.WriteLine("Position has been finalized.");
+    }
+}
